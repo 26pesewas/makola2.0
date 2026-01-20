94d3f632f50437f4d19cbabb5f39b899abb4e4c2
26pesewas <naaakuokor@gmail.com>
2026-01-20 23:51:54 +0000

    uploaded new images

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());
